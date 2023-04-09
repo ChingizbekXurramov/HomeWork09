4ad3255ae6e2a234ffdce781a71eb763527580fd
Chingizbek <128830130+ChingizbekXurramov@users.noreply.github.com>
2023-04-09 22:25:07 +0300

    task3

---
+++ task3/task3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your number==> ");
+            string number = Console.ReadLine();
+            int row;
+            bool check = int.TryParse(number, out row);
+
+            for (int i = 1; i <= row; i++)
+            {
+                for (int j = 1; j <= row - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+
+            Main(args);
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
