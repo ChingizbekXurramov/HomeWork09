f4bd73b8c06e63a984e2099d06616db0f6d8fffa
Chingizbek <128830130+ChingizbekXurramov@users.noreply.github.com>
2023-04-09 22:32:04 +0300

    task2

---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your number==> ");
+            string number = Console.ReadLine();
+            int array;
+            bool check = int.TryParse(input, out array);
+            int[] arr = new int[array];
+
+
+            Random random = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int k = random.Next(1, 10);
+                Console.Write(k + " ");
+                arr[i] = k;
+            }
+            Console.WriteLine();
+
+
+            Array.Sort(arr);
+
+            Console.WriteLine("Sort array : ");
+
+            foreach (int num in arr)
+            {
+                Console.Write(num + ", ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
