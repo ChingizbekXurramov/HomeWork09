5f0b9659fddb55abbc492ab3a7a64934fc887825
Chingizbek <128830130+ChingizbekXurramov@users.noreply.github.com>
2023-04-09 21:46:57 +0300

    task5

---
+++ Homework/Homework/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input your String : ");
+            string str1 = Console.ReadLine();
+            Console.Write("+Input your String : ");
+            string str2 = Console.ReadLine();
+
+            for (int i = 1; i < str1.Length + str2.Length; i++)
+            {
+                if (i ==    str1.Length) continue;
+                else Console.Write((str1 + str2)[i]);
+            }
+
+            Console.ReadLine();
+
+        }
+        
+    }
+}
